b075ff307d5f804441f052ed237daff630974200
sandeepreddy <shegursandeepreddy@gmail.com>
2023-07-25 23:21:49 -0400

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Sandeep Reddy on 2023-07-25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
